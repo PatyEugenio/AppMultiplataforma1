e96a89bb0f08372382ee5dd46c5e90a5500e37e3
PatyEugenio <patyEugelope@gmail.com>
2024-05-30 00:15:37 -0600

    Primer commit del proyecto

---
+++ src/jvmMain/kotlin/me.patye.application/Server.kt
@@ -0,0 +1,38 @@
+package me.patye.application
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.html.*
+import io.ktor.server.http.content.*
+import io.ktor.server.netty.Netty
+import io.ktor.server.routing.*
+import kotlinx.html.*
+
+fun HTML.index() {
+    head {
+        title("Hola Bienvenido!")
+    }
+    body {
+        div {
+            +"Hola Bienvenido"
+        }
+        div {
+            id = "root"
+        }
+        script(src = "/static/AppMultiplataforma.js") {}
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+        routing {
+            get("/") {
+                call.respondHtml(HttpStatusCode.OK, HTML::index)
+            }
+            static("/static") {
+                resources()
+            }
+        }
+    }.start(wait = true)
+}
